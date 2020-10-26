c883fdb1c7907f2a337bdd34ace179885eb4ca2c
maxgrt <38078000+maxgrt@users.noreply.github.com>
2020-10-26 22:25:40 +0300

    Create update.py

---
+++ src/update.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import zipfile
+import shutil
+import os
+
+print("BLauncher is updating now...")
+z = zipfile.ZipFile("update.zip", "r")
+print("Extracting files...")
+z.extractall()
+print("Copying files...")
+shutil.copyfile("BLauncher-master/src/main.py", "main.py")
+shutil.copyfile("BLauncher-master/src/lang.py", "lang.py")
+print("Deleting unnecessary files...")
+os.remove("update.zip")
+shutil.rmtree("BLauncher-master")
+print("Sucsess!")
