58de49037748d7211c23d9fa9b78efdfd4b1d283
maxgrt <38078000+maxgrt@users.noreply.github.com>
2021-01-06 19:54:49 +0300

    Update lang.py

--- src/lang.py
+++ src/lang.py
@@ -1,14 +1,17 @@
 # This is language file. You can change it!
-
-mojang_login = "Log in through Mojang"
-login = "Login"
-password = "Password"
-log_in = "Log in"
-nick = "Nick"
-uuid = "UUID"
-access_token = "Token"
-installed = "Installed"
-ready_to_install = "Ready to install"
-install_run = "Run"
-error = "Error"
-version_is_not_detected = "Invalid version"
+nick = 'Nick'
+installed = 'Installed'
+ready_to_install = 'Ready to install'
+install_run = 'Run'
+error = 'Error'
+account = 'Account'
+version = 'Version'
+run = 'Run'
+settings = 'Settings'
+exit = 'Exit'
+file = 'File'
+version_is_not_detected = 'Invalid version'
+mojang_login = 'Log in through Mojang'
+ask_mojang_login = 'Enter Mojang login'
+ask_mojang_password = 'Enter Mojang password'
+error_during_login = 'Error! Login or Password is incorrect!'
